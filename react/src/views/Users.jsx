0cbe87ba2e1a801a42ba9f8cb6598cf0e67e9022
pa799865 <pa799865@gmail.com>
2025-07-18 14:11:26 +0700

    users crud in API

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 export default function Users() {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = () => {axiosClient.get('/users').then(({data}) => {setLoading(false)
+        console.log(data)}).catch(() => {setLoading(false)})}
+
     return (
         <div>
             Users
