9e0edc6f6ba6be8b7f7cfd40bd700aef274e2235
pa799865 <pa799865@gmail.com>
2025-07-19 23:17:03 +0700

    crud in frontend(Read)

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,6 @@
-import axios from "axios";
 import { useEffect, useState } from "react"
+import axiosClient from "../axios-client";
+import { Link } from "react-router-dom";
 
 export default function Users() {
 
@@ -10,12 +11,53 @@ export default function Users() {
         getUsers();
     }, [])
 
-    const getUsers = () => {axiosClient.get('/users').then(({data}) => {setLoading(false)
-        console.log(data)}).catch(() => {setLoading(false)})}
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({data}) => {
+                setLoading(false)
+                console.log(data)
+                setUsers(data.data)
+            })
+            .catch(() => {setLoading(false)})
+    }
 
     return (
         <div>
-            Users
+        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+            <h1>Users</h1>
+            <Link to='/users/new' className="btn-add">Add new</Link>
+        </div>
+        <div className="card animated fadeInDown">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Create Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(u => {
+                        return (
+                        <tr>
+                            <td>{u.id}</td>
+                            <td>{u.name}</td>
+                            <td>{u.email}</td>
+                            <td>{u.created_at}</td>
+                            <td>
+                                <Link to={'/users/'+u.id} className="btn-edit">Edit</Link>
+                                &nbsp;
+                                <button className="btn-delete">Delete</button>
+                            </td>
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
         </div>
     )
 }
